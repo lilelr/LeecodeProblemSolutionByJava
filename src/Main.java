dddd3e4e0fca3e919f75d740f92e2d55ac734929
lilelr <lilelr@163.com>
2016-03-31 20:21:49 +0800

    301

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import problems.RangeSumQuery303.NumArray;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        NumArray numArray = new NumArray(new int[]{-2,0,3,-5,2,-1});
+
+        System.out.println(numArray.sumRange(2,5));
+        System.out.println(numArray.sumRange(0,2));
+        System.out.println(numArray.sumRange(0,5));
+
+
     }
 }
---
+++ src/problems/RemoveInvalidParentheses301/Solution.java
@@ -0,0 +1,12 @@
+package problems.RemoveInvalidParentheses301;
+
+import java.util.List;
+
+/**
+ * Created by yuxiao on 3/31/16.
+ */
+public class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        return null;
+    }
+}
