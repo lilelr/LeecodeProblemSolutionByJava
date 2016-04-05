1feb6ada9e3de68d1a9131b759f988279f024286
lilelr <lilelr@163.com>
2016-04-05 16:17:22 +0800

    207 first

---
+++ src/problems/CourseSchedule207/Solution.java
@@ -0,0 +1,11 @@
+package problems.CourseSchedule207;
+
+/**
+ * Created by yuxiao on 4/5/16.
+ */
+public class Solution {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        return true;
+    }
+}
