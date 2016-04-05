6f51482dccbed5c218023e9093fa15ddde9757dc
lilelr <lilelr@163.com>
2016-04-05 13:34:22 +0800

    310 first

---
+++ src/problems/MinimumHeightTrees310/Solution.java
@@ -0,0 +1,12 @@
+package problems.MinimumHeightTrees310;
+
+import java.util.List;
+
+/**
+ * Created by yuxiao on 4/5/16.
+ */
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+            return null;
+    }
+}
