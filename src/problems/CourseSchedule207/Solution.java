2f8342bfc2f3f2facdd702e530f1d8675d53517a
lilelr <lilelr@163.com>
2016-04-05 17:08:05 +0800

    Code format

--- src/problems/CourseSchedule207/Solution.java
+++ src/problems/CourseSchedule207/Solution.java
@@ -2,7 +2,6 @@ package problems.CourseSchedule207;
 
 import org.junit.Test;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -14,19 +13,19 @@ public class Solution {
 
     /// DAG   TopologicalSort O( V+E )
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(numCourses<=1) return true;
+        if (numCourses <= 1) return true;
 
 
         //build graph and indegree array
         List<Integer>[] graph = new ArrayList[numCourses];
         int[] indegree = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
+        for (int i = 0; i < numCourses; i++) {
             indegree[i] = 0;
         }
-        for(int i=0;i<numCourses;i++){
+        for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
         }
-        for (int i=0;i<prerequisites.length;i++){
+        for (int i = 0; i < prerequisites.length; i++) {
             int v1 = prerequisites[i][0];
             int v2 = prerequisites[i][1];
 
@@ -35,37 +34,37 @@ public class Solution {
         }
 
         Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i] ==0){
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
                 stack.push(Integer.valueOf(i));
             }
         }
-        int count=0;
-        while(!stack.empty()){
+        int count = 0;
+        while (!stack.empty()) {
             int popval = stack.pop();
             count++;
             int tmpsize = graph[popval].size();
-            for(int i=0;i<tmpsize;i++){
+            for (int i = 0; i < tmpsize; i++) {
                 int ver = graph[popval].get(i);
                 indegree[ver]--;
-                if(indegree[ver] == 0){
+                if (indegree[ver] == 0) {
                     stack.push(ver);
                 }
             }
         }
 
-        if(count < numCourses) return false;
+        if (count < numCourses) return false;
         return true;
     }
 
 
     @Test
-    public void testcanFinish(){
+    public void testcanFinish() {
         int[][] edges = new int[2][2];
         edges[0][0] = 1;
-        edges[0][1]  = 0;
-        edges[1][0] =0 ;
+        edges[0][1] = 0;
+        edges[1][0] = 0;
         edges[1][1] = 1;
-        System.out.println(canFinish(2,edges));
+        System.out.println(canFinish(2, edges));
     }
 }
--- src/problems/CourseSchedule210/Solution.java
+++ src/problems/CourseSchedule210/Solution.java
@@ -12,20 +12,20 @@ public class Solution {
     /// DAG   TopologicalSort O( V+E )
     public int[] findOrder(int numCourses, int[][] prerequisites) {
 
-        if (numCourses==0) return new int[0];
-        if(numCourses==1) return new int[]{0};
+        if (numCourses == 0) return new int[0];
+        if (numCourses == 1) return new int[]{0};
 
         int[] ans = new int[numCourses];
         //build graph and indegree array
         List<Integer>[] graph = new ArrayList[numCourses];
         int[] indegree = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
+        for (int i = 0; i < numCourses; i++) {
             indegree[i] = 0;
         }
-        for(int i=0;i<numCourses;i++){
+        for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
         }
-        for (int i=0;i<prerequisites.length;i++){
+        for (int i = 0; i < prerequisites.length; i++) {
             int v1 = prerequisites[i][0];
             int v2 = prerequisites[i][1];
 
@@ -34,27 +34,27 @@ public class Solution {
         }
 
         Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i] ==0){
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
                 stack.push(Integer.valueOf(i));
             }
         }
-        int count=0;
-        while(!stack.empty()){
+        int count = 0;
+        while (!stack.empty()) {
             int popval = stack.pop();
             ans[count] = popval;
             count++;
             int tmpsize = graph[popval].size();
-            for(int i=0;i<tmpsize;i++){
+            for (int i = 0; i < tmpsize; i++) {
                 int ver = graph[popval].get(i);
                 indegree[ver]--;
-                if(indegree[ver] == 0){
+                if (indegree[ver] == 0) {
                     stack.push(ver);
                 }
             }
         }
 
-        if(count < numCourses) return  new int[0];
+        if (count < numCourses) return new int[0];
         return ans;
     }
 }
