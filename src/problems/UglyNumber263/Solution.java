6391bea5ff022bbad4a82575e75af80f795bea7b
lilelr <lilelr@163.com>
2016-04-05 17:44:57 +0800

    263 over

---
+++ src/problems/UglyNumber263/Solution.java
@@ -0,0 +1,33 @@
+package problems.UglyNumber263;
+
+import org.junit.Test;
+
+/**
+ * Created by yuxiao on 4/5/16.
+ */
+public class Solution {
+    public boolean isUgly(int num) {
+        if(num == 1) return  true;
+        if (num == 0) return false;
+
+        int[] factors = {2,3,5};
+        while (true){
+
+            if(num%2 != 0 && num%3!=0 && num%5!=0){
+                return false;
+            }
+            for (int item:factors){
+                if(num%item == 0){
+                    num = num/item;
+                }
+            }
+            if(num == 1) return true;
+
+        }
+    }
+
+    @Test
+    public void testIsUgly(){
+        System.out.println(isUgly(8));
+    }
+}
