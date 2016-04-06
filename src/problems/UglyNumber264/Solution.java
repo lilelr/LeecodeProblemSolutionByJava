5a108e5b6e2d731f371c112d3ffda6194e68b107
lilelr <lilelr@163.com>
2016-04-06 16:19:57 +0800

    264  over

--- src/problems/UglyNumber264/Solution.java
+++ src/problems/UglyNumber264/Solution.java
@@ -11,86 +11,43 @@ import java.util.List;
  */
 public class Solution {
 
-    private  List<Integer> numList = new ArrayList<>();
 
+
+    /// Dynamic Programming
     public int nthUglyNumber(int n) {
-        if (n==1) return 1;
-        numList.add(1);
-        numList.add(2);
-        numList.add(3);
-        numList.add(4);
-        numList.add(5);
-        int numsize = numList.size();
-        if(numsize>=n){
-            return numList.get(n-1);
-        }
+        if(n==1) return 1;
+        List<Integer> ans = new ArrayList<>();
+        ans.add(1);
+        int i2=0,i3=0,i5=0;
+        int count=1;
+        while(count<=n){
+            boolean flag=true;
+            int nextItem = Math.min(ans.get(i2)*2,Math.min(ans.get(i3)*3,ans.get(i5)*5));
+            if(nextItem == ans.get(i2)*2){
+                i2++;
+            }
+            if(nextItem == ans.get(i3)*3){
+                i3++;
 
+            }
+            if(nextItem == ans.get(i5)*5){
+                i5++;
 
-        int pos=2;
-        while (numsize<n){
-
-            int[] temparr = new int[9];
-            int temppos=0;
-            for(int i=0;i<3;i++){
-                temparr[temppos++]=(this.numList.get(pos+i)*2);
-                temparr[temppos++]=(this.numList.get(pos+i)*3);
-                temparr[temppos++]=(this.numList.get(pos+i)*5);
             }
 
-            Arrays.sort(temparr);
-            for(int i=0;i<9;i++){
-                if (this.numList.contains(temparr[i])) continue;
-                this.numList.add(temparr[i]);
-            }
-            numsize = numList.size();
-            pos += 1;
+                count++;
+                ans.add(nextItem);
+
+
         }
-
-        return numList.get(n-1);
-
-//        int pos=1;
-//        int limit = n+30;
-//        while (numsize<limit){
-//            merge(pos,numList.get(pos));
-//            pos++;
-//            numsize = numList.size();
-//        }
-//
-//        return numList.get(n-1);
-
-
-
-
+        return ans.get(n-1);
     }
 
-
-
-
-    private void merge(int pos,int posItem){
-        int[] temparr = {posItem*2,posItem*3,posItem*5};
-        for(int i=0;i<3;i++){
-            int temp = temparr[i];
-            if(this.numList.contains(temp)) continue;
-
-            int len = this.numList.size();
-            boolean flag = false;
-            for(int j=pos+1;j<len;j++){
-
-                 if(this.numList.get(j) > temp){
-                    flag = true;
-                    this.numList.add(j,temp);
-                     break;
-                }
-            }
-            if(!flag){
-                this.numList.add(temp);
-            }
-        }
-    }
 
 
     @Test
     public void testnthUglyNumber(){
+
         System.out.println(nthUglyNumber(12));
     }
 }
