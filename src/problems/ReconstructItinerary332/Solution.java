b383dcc30576cae8fbe37cc30ccb432ce162cc6d
lilelr <lilelr@163.com>
2016-04-07 11:34:08 +0800

    332 initial

---
+++ src/problems/ReconstructItinerary332/Solution.java
@@ -0,0 +1,14 @@
+package problems.ReconstructItinerary332;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yuxiao on 4/7/16.  Graph DFS
+ */
+public class Solution {
+    public List<String> findItinerary(String[][] tickets) {
+        List<String> ret = new ArrayList<>();
+        return ret;
+    }
+}
