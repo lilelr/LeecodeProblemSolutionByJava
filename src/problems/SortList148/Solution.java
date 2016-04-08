bd0d7df3ce324fd5f3e8a84f3af688fbd28d2aed
lilelr <lilelr@163.com>
2016-04-08 14:04:22 +0800

    148 initial

---
+++ src/problems/SortList148/Solution.java
@@ -0,0 +1,23 @@
+package problems.SortList148;
+
+/**
+ * Created by yuxiao on 4/8/16.
+ */
+
+
+
+public class Solution {
+
+
+      //Definition for singly-linked list.
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+      }
+
+    public ListNode sortList(ListNode head) {
+            return head;
+    }
+
+}
