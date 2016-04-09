d4412d24d7ecd4440d3aae44f86e51a5d44cfc37
lilelr <lilelr@163.com>
2016-04-09 15:47:19 +0800

    146 ac

--- src/problems/LRUCache146/LRUCache.java
+++ src/problems/LRUCache146/LRUCache.java
@@ -7,12 +7,17 @@ import java.util.LinkedList;
 
 /**
  * Created by yuxiao on 4/9/16.
+ * Using HashMap Algorithm Complexity O(logn)
+ * When detaching a node from Double-Link List after knowing the node from hash-map,
+ * it costs only O(1).
+ *
  */
 public class LRUCache {
 
     private int mcapacity;
     private ListNode head;
     private int curSize;
+    private HashMap<Integer,ListNode> cacheMap;
 
     //Definition for Double-linked list.
     public class ListNode {
@@ -33,57 +38,70 @@ public class LRUCache {
         this.head.next = this.head;
         this.head.pre = this.head;
         this.curSize = 0;
+        this.cacheMap = new HashMap<>();
     }
 
     public int get(int key) {
-        ListNode tmpNode = this.getNode(key);
+        ListNode tmpNode = this.cacheMap.get(key);
         if (tmpNode != null) {
+            this.detach(tmpNode);
             this.insertStartNode(tmpNode);
             return tmpNode.val;
         } else {
-            return -2;
+            return -1;
         }
 
+    }
+
+    public void detach(ListNode node){
+        ListNode preOfNode = node.pre;
+        ListNode nextOfNode = node.next;
+        preOfNode.next = nextOfNode;
+        nextOfNode.pre = preOfNode;
     }
 
     public void set(int key, int value) {
         if(this.mcapacity==0) return;
 
-        ListNode tmpNode = this.getNode(key);
+        ListNode tmpNode = this.cacheMap.get(key);
         if (tmpNode != null) {
+            this.detach(tmpNode);
             tmpNode.val = value;
             this.insertStartNode(tmpNode);
         } else {
             if(this.curSize <this.mcapacity){
                 ListNode newNode = new ListNode(key,value);
+                this.cacheMap.put(key,newNode);
                 this.insertStartNode(newNode);
                 this.curSize++;
             }else{
                 //curSize == mcapicity
                ListNode tailNode= this.removeEndNode();
+                this.cacheMap.remove(tailNode.key);
                 tailNode.key = key;
                 tailNode.val = value;
                 this.insertStartNode(tailNode);
+                this.cacheMap.put(tailNode.key,tailNode);
 
             }
         }
     }
 
-    public ListNode getNode(int key) {
-        ListNode p = this.head.next;
-        while (p != head) {
-            if (p.key == key) {
-                ListNode preOfP = p.pre;
-                ListNode nextOfP = p.next;
-                nextOfP.pre = preOfP;
-                preOfP.next = nextOfP;
-                p.pre = p.next = null;
-                return p;
-            }
-            p = p.next;
-        }
-        return null;
-    }
+//    public ListNode getNode(int key) {
+//        ListNode p = this.head.next;
+//        while (p != head) {
+//            if (p.key == key) {
+//                ListNode preOfP = p.pre;
+//                ListNode nextOfP = p.next;
+//                nextOfP.pre = preOfP;
+//                preOfP.next = nextOfP;
+//                p.pre = p.next = null;
+//                return p;
+//            }
+//            p = p.next;
+//        }
+//        return null;
+//    }
 
     public void insertStartNode(ListNode newNode) {
         ListNode oldStart = this.head.next;
