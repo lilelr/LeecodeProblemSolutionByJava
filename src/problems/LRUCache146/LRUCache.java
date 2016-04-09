c35435257bb5766487ba6854f36bf99e3f46c4cd
lilelr <lilelr@163.com>
2016-04-09 15:33:34 +0800

    146  LRU cache time limited 2 Double Linked List implemented by me.

--- src/problems/LRUCache146/LRUCache.java
+++ src/problems/LRUCache146/LRUCache.java
@@ -11,61 +11,116 @@ import java.util.LinkedList;
 public class LRUCache {
 
     private int mcapacity;
-    private LinkedList<Integer> cacheNums;
-    private HashMap<Integer,Integer> cacheMap;
+    private ListNode head;
     private int curSize;
+
+    //Definition for Double-linked list.
+    public class ListNode {
+        int val;
+        int key;
+        ListNode next;
+        ListNode pre;
+
+        ListNode(int x, int y) {
+            key = x;
+            val = y;
+        }
+    }
 
     public LRUCache(int capacity) {
         this.mcapacity = capacity;
-        this.cacheNums = new LinkedList<Integer>();
-        this.cacheMap = new HashMap<>();
-        this.curSize=0;
+        this.head = new ListNode(0, 0);
+        this.head.next = this.head;
+        this.head.pre = this.head;
+        this.curSize = 0;
     }
 
     public int get(int key) {
-        if(this.cacheNums.remove(Integer.valueOf(key))){
-           this.cacheNums.push(Integer.valueOf(key));
-            return this.cacheMap.get(Integer.valueOf(key));
-        } else{
-            return -1;
+        ListNode tmpNode = this.getNode(key);
+        if (tmpNode != null) {
+            this.insertStartNode(tmpNode);
+            return tmpNode.val;
+        } else {
+            return -2;
         }
+
     }
 
     public void set(int key, int value) {
-        Integer keyCopy = Integer.valueOf(key);
-        if(this.cacheNums.remove(keyCopy)){
-            this.cacheNums.push(keyCopy);
-            this.cacheMap.replace(keyCopy, value);
-        } else{
-            if(this.curSize < this.mcapacity){
-                this.curSize++;
-                this.cacheNums.push(keyCopy);
-                this.cacheMap.put(keyCopy,value);
-            } else{
-                Integer remKey=this.cacheNums.removeLast();
-                this.cacheNums.push(keyCopy);
-                this.cacheMap.remove(remKey);
-                this.cacheMap.put(keyCopy,value);
-            }
+        if(this.mcapacity==0) return;
 
+        ListNode tmpNode = this.getNode(key);
+        if (tmpNode != null) {
+            tmpNode.val = value;
+            this.insertStartNode(tmpNode);
+        } else {
+            if(this.curSize <this.mcapacity){
+                ListNode newNode = new ListNode(key,value);
+                this.insertStartNode(newNode);
+                this.curSize++;
+            }else{
+                //curSize == mcapicity
+               ListNode tailNode= this.removeEndNode();
+                tailNode.key = key;
+                tailNode.val = value;
+                this.insertStartNode(tailNode);
+
+            }
         }
     }
 
-    public static void main(String[] args){
+    public ListNode getNode(int key) {
+        ListNode p = this.head.next;
+        while (p != head) {
+            if (p.key == key) {
+                ListNode preOfP = p.pre;
+                ListNode nextOfP = p.next;
+                nextOfP.pre = preOfP;
+                preOfP.next = nextOfP;
+                p.pre = p.next = null;
+                return p;
+            }
+            p = p.next;
+        }
+        return null;
+    }
+
+    public void insertStartNode(ListNode newNode) {
+        ListNode oldStart = this.head.next;
+        newNode.next = oldStart;
+        oldStart.pre = newNode;
+        this.head.next = newNode;
+        newNode.pre = this.head;
+    }
+
+    public ListNode removeEndNode() {
+        ListNode tail = this.head.pre;
+        ListNode tailPre = tail.pre;
+        tailPre.next = this.head;
+        this.head.pre = tailPre;
+        tail.next = tail.pre = null;
+        return tail;
+    }
+
+    public static void main(String[] args) {
         LRUCache lruCache = new LRUCache(3);
-        lruCache.set(1,-1);
-        lruCache.set(3,-3);
+        lruCache.set(1, -1);
+        lruCache.set(3, -3);
         int tmpval = lruCache.get(3);
-        lruCache.set(2,-2);
+        lruCache.set(2, -2);
+        lruCache.set(4,-4);
+        lruCache.get(4);
+        lruCache.set(5,-5);
+
     }
 
     @Test
-    public void testLRUCache(){
+    public void testLRUCache() {
         LRUCache lruCache = new LRUCache(3);
-        lruCache.set(1,-1);
-        lruCache.set(3,-3);
+        lruCache.set(1, -1);
+        lruCache.set(3, -3);
         int tmpval = lruCache.get(3);
-        lruCache.set(2,-2);
+        lruCache.set(2, -2);
 
     }
 }
