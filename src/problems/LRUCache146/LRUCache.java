8b65a2f3b1c01e1f70f5b4b8cb435a677e81dbf4
lilelr <lilelr@163.com>
2016-04-09 11:11:37 +0800

    146 initial

---
+++ src/problems/LRUCache146/LRUCache.java
@@ -0,0 +1,22 @@
+package problems.LRUCache146;
+
+/**
+ * Created by yuxiao on 4/9/16.
+ */
+public class LRUCache {
+
+    private int mcapacity;
+    
+
+    public LRUCache(int capacity) {
+        this.mcapacity = capacity;
+    }
+
+    public int get(int key) {
+
+    }
+
+    public void set(int key, int value) {
+
+    }
+}
