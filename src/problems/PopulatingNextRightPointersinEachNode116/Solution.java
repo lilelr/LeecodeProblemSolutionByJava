6401d1ae196cf2950dc17c07f552c5f28bf0d6ec
lilelr <lilelr@163.com>
2016-04-15 16:22:18 +0800

    117 ac

--- src/problems/PopulatingNextRightPointersinEachNode116/Solution.java
+++ src/problems/PopulatingNextRightPointersinEachNode116/Solution.java
@@ -10,51 +10,60 @@ import java.util.Queue;
  */
 public class Solution {
 
-     //* Definition for binary tree with next pointer.
-      public class TreeLinkNode {
-          int val;
-          TreeLinkNode left, right, next;
-          TreeLinkNode(int x) { val = x; }
-      }
+    //* Definition for binary tree with next pointer.
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
+
+        TreeLinkNode(int x) {
+            val = x;
+        }
+    }
 
     // level traversal
     public TreeLinkNode connect(TreeLinkNode root) {
 
-        if(root == null) return null;
-        int last=0;
+        if (root == null) return null;
+        int last;
         LinkedList<TreeLinkNode> queue = new LinkedList<>();
         queue.push(root);
-        int front=0,rear=0;
+        int front = 0, rear = 0;
         rear++;
-        last=1;
-        boolean flag=false;
+        last = 1;
+        boolean flag = false;
         TreeLinkNode preNode = new TreeLinkNode(0);
-        while (front<rear){
+        while (front < rear) {
             front++;
             TreeLinkNode frontNode = queue.pop();
 
 
-            if(frontNode.left != null){
+            if (frontNode.left != null) {
                 queue.offer(frontNode.left);
                 rear++;
             }
 
-            if(frontNode.right != null){
+            if (frontNode.right != null) {
                 queue.offer(frontNode.right);
                 rear++;
             }
 
-            if(last == front ){
+            if (frontNode.val == 4) {
+                System.out.println("ff");
+                int yy;
+            }
+            if (!flag) {
                 preNode.next = frontNode;
+            }
+
+            if (last == front) {
+
                 frontNode.next = null;
                 flag = true;
                 last = rear;
-            } else{
-                if(!flag){
-                    preNode.next = frontNode;
-                }
+            } else {
+
                 preNode = frontNode;
-                flag=false;
+                flag = false;
             }
         }
 
@@ -62,16 +71,21 @@ public class Solution {
     }
 
     @Test
-    public void test(){
-        TreeLinkNode root = new TreeLinkNode(0);
-        TreeLinkNode first = new TreeLinkNode(1);
-        TreeLinkNode sec = new TreeLinkNode(2);
-        TreeLinkNode third = new TreeLinkNode(3);
-        TreeLinkNode forth = new TreeLinkNode(4);
+    public void test() {
+        TreeLinkNode root = new TreeLinkNode(1);
+        TreeLinkNode first = new TreeLinkNode(2);
+        TreeLinkNode sec = new TreeLinkNode(3);
+        TreeLinkNode third = new TreeLinkNode(4);
+        TreeLinkNode forth = new TreeLinkNode(5);
+        TreeLinkNode fifth = new TreeLinkNode(6);
         root.left = first;
         root.right = sec;
-        first.left =third;
-        first.right = forth;
+        first.left = third;
+//        first.right = forth;
+//        sec.right = fifth;
+
+        sec.right = forth;
+        forth.right = fifth;
         root = connect(root);
     }
 
