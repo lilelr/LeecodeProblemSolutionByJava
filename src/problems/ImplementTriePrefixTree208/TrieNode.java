6c4adc2ce483103d4b10d97091b7ec59ebde2bf0
lilelr <lilelr@163.com>
2016-04-18 17:46:20 +0800

    Trie init

---
+++ src/problems/ImplementTriePrefixTree208/TrieNode.java
@@ -0,0 +1,40 @@
+package problems.ImplementTriePrefixTree208;
+
+/**
+ * Created by yuxiao on 16/4/18.
+ */
+class TrieNode {
+    // Initialize your data structure here.
+    public TrieNode() {
+
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+
+    }
+}
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie = new Trie();
+// trie.insert("somestring");
+// trie.search("key");
