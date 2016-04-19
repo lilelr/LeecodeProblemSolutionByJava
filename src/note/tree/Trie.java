c27a7e0f1d07bfb7b5ea8272405698951c945d7d
lilelr <lilelr@163.com>
2016-04-19 11:39:59 +0800

    208 trie ac

---
+++ src/note/tree/Trie.java
@@ -0,0 +1,52 @@
+package note.tree;
+
+/**
+ * Created by yuxiao on 16/4/19.
+ */
+class TrieNode {
+    boolean isEnd;
+    TrieNode[] children;
+
+    public TrieNode() {
+        isEnd = true;
+        children = new TrieNode[26];
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+        for(int i=0, L=word.length(); i<L; i++) {
+            int id = word.charAt(i) - 'a';
+            if(current.children[id]==null) {
+                current.children[id] = new TrieNode();
+                current.children[id].isEnd = false;
+            }
+            current = current.children[id];
+        }
+        current.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return search(word, 1);
+    }
+    public boolean startsWith(String prefix) {
+        return search(prefix, 2);
+    }
+    private boolean search(String str, int type) {
+        TrieNode current = root;
+        int i=-1, L=str.length();
+        while(++i<L) {
+            int id = str.charAt(i) - 'a';
+            if((current=current.children[id]) == null) return false;
+        }
+        return type==1 ? current.isEnd : true;
+    }
+}
+
