f4a6c7337da366be35c443c0e95e227aa3ef5bdd
lilelr <lilelr@163.com>
2016-04-26 15:05:42 +0800

    220 binary search tree add and delete ac

--- src/note/binarysearch/ContainsDuplicate220/Solution.java
+++ src/note/binarysearch/ContainsDuplicate220/Solution.java
@@ -1,26 +1,99 @@
 package note.binarysearch.ContainsDuplicate220;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by yuxiao on 4/26/16.
  */
 public class Solution {
+    public class TreeNode{
+        public long val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(long x){
+            val = x;
+        }
+    }
+
+    private TreeNode add(TreeNode root, TreeNode nNode) {
+        if(root == null) {
+            return nNode;
+        }
+        else if(root.val < nNode.val) {
+            root.right = add(root.right, nNode);
+            return root;
+        }
+        else {
+            root.left = add(root.left, nNode);
+            return root;
+        }
+    }
+
+    private TreeNode delete(TreeNode root, TreeNode dNode) {
+        if(root == null) {
+            return null;
+        }
+        else if(root.val < dNode.val) {
+            root.right = delete(root.right, dNode);
+            return root;
+        }
+        else if(root.val > dNode.val) {
+            root.left = delete(root.left, dNode);
+            return root;
+        }
+        else if(root == dNode) {
+            if(dNode.left == null && dNode.right == null) return null;
+            else if(dNode.left != null && dNode.right == null) return dNode.left;
+            else if(dNode.right != null && dNode.left == null) return dNode.right;
+            else {
+                TreeNode p = dNode.right;
+                while(p.left != null) p = p.left;
+                dNode.right = delete(dNode.right, p);
+                p.left = dNode.left;
+                p.right = dNode.right;
+                return p;
+            }
+        }
+        else {
+            return root;
+        }
+    }
+
+    private boolean search(TreeNode root, long val, int t) {
+        if(root == null) {
+            return false;
+        }
+        else if(Math.abs((root.val - val)) <= t) {
+            return true;
+        }
+        else if((root.val - val) > t) {
+            return search(root.left, val, t);
+        }
+        else {
+            return search(root.right, val, t);
+        }
+    }
+
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-//        Map<Integer,Integer> map =new HashMap<>();
-//        for(int i=0;i<nums.length;i++){
-//            if(map.containsKey(nums[i]) && i-map.get([i])<=k){
-//                return true;
-//            }
-//
-//            if(map.size() > k){
-//                map.remove(nums[i-k]);
-//            }
-//            map.put(nums[i],i);
-//        }
+        if(k < 1 || t < 0 || nums.length <= 1) {
+            return false;
+        }
+        int len = nums.length;
+        TreeNode[] map = new TreeNode[len];
+        map[0] = new TreeNode((long)nums[0]);
+        TreeNode root = null;
+        root = add(root, map[0]);
+        for(int i = 1; i < len; i++) {
+            if(search(root, (long)nums[i], t)) {
+                return true;
+            }
+            map[i] = new TreeNode((long)nums[i]);
+            if(i - k >= 0) {
+                root = delete(root, map[i-k]);
+            }
+            root = add(root, map[i]);
+        }
+        return false;
     }
 
     //219 hashmap or hashset
