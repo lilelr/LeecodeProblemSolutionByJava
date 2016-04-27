606f633522f7d9adea98c8082e54a82d38a7cbee
lilelr <lilelr@163.com>
2016-04-27 15:51:19 +0800

    239 solution2

--- src/note/heap/SlidingWindowMaximum239/Solution.java
+++ src/note/heap/SlidingWindowMaximum239/Solution.java
@@ -14,6 +14,8 @@ public class Solution {
         if(k == 1 || k==0) return nums;
         List<Integer> res = new ArrayList<>();
         PriorityQueue<Num> pq = new PriorityQueue<>();
+        // another method PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
         for(int i=0;i<k;i++){
             pq.offer(new Num(nums[i],i));
         }
@@ -62,6 +64,51 @@ public class Solution {
         public int compareTo(Num that) {
             return that.val - this.val;
         }
+    }
+
+    //Solution2  4ms
+    public int[] maxSlidingWindow2(int[] nums,int k){
+        int max = 0; //maximum of k elements in window
+
+        int len = nums.length ;
+        int [] result;
+        if(len == 1|| k==0){
+            return nums;
+        }
+        len = len- k +1; // loop will execute 'len' times.
+
+        result = new int[len];
+
+        for(int i =0; i < len ; i++){
+            if(i==0){
+                max = getMax(nums,i,i+k-1);  //linear search to get maximum of k values
+            }
+            else{
+                if(max == nums[i-1]){   //check whether the last number is max or not
+                    max =  getMax(nums,i,i+k-1); // get the new Max number
+                }
+                else if( max < nums[i+k-1]){ // whether previous max is less than the new kth number
+                    max = nums[i+k-1];
+                }
+            }
+            result[i]=max;
+
+        }
+
+        return result;
+    }
+
+
+
+
+    private static int getMax(int[] nums, int i, int w) {
+        int max = nums[i];
+
+        for(int j = i+1; j<= w ; j++){
+            if(max < nums[j])
+                max = nums[j];
+        }
+        return max;
     }
 
 
