82cf294b60f4c26dfb2ce8131e47efc765089327
lilelr <lilelr@163.com>
2016-06-04 11:18:35 +0800

    H-Index 274  bucket sort

---
+++ src/note/sort/HIndex274/Solution.java
@@ -0,0 +1,44 @@
+package note.sort.HIndex274;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by yuxiao on 16/6/4.
+ */
+public class Solution {
+    // bucket sort
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        int[] buckets = new int[len+1];
+        for(int i=0;i<len;i++){
+            if(citations[i] >= len){
+                buckets[len]++;
+            } else{
+                buckets[citations[i]]++;
+            }
+        }
+
+        int count=0;
+        int res=0;
+        for(int i=0;i<=len;i++){
+            count+= buckets[i];
+            int moreThanCur = len - count+buckets[i];
+            if(moreThanCur >= i){
+                res = Math.max(res,i);
+            }
+        }
+//        for(int val:buckets){
+//            System.out.println(val);
+//        }
+//        System.out.println(res);
+        return res;
+    }
+
+    @Test
+    public void test(){
+        int[] citations = {1,6,6,6,6,3,4};
+        hIndex(citations);
+    }
+}
