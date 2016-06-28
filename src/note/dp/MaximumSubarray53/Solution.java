3ff22ed4761fc517078c9ed0b48258125afe04ae
lilelr <lilelr@163.com>
2016-06-28 09:46:17 +0800

    53. Maximum Subarray   dp

---
+++ src/note/dp/MaximumSubarray53/Solution.java
@@ -0,0 +1,28 @@
+package note.dp.MaximumSubarray53;
+
+import org.junit.Test;
+
+/**
+ * Created by yuxiao on 6/23/16.
+ */
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int tempSub = 0;
+        for(int i=0;i<nums.length;i++){
+            tempSub+=nums[i];
+            res = Math.max(tempSub,res);
+            res = Math.max(nums[i],res);
+            if(tempSub<0){
+                tempSub = 0;
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void test(){
+        int[] nums = {-2,1,-3,4,-1,2,1};
+        System.out.println(maxSubArray(nums));
+    }
+}
