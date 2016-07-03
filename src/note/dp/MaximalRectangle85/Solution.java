ac398e8e346a52da982def15ccaf65882a78b547
lilelr <lilelr@163.com>
2016-07-03 14:05:32 +0800

    85. Maximal Rectangle    stack  and dp

--- src/note/dp/MaximalRectangle85/Solution.java
+++ src/note/dp/MaximalRectangle85/Solution.java
@@ -1,20 +1,33 @@
 package note.dp.MaximalRectangle85;
 
+import org.junit.Test;
+
 import java.sql.Struct;
+import java.util.Stack;
 
 /**
  * Created by yuxiao on 7/3/16.
  */
 public class Solution {
-
-    public class Cell {
-        public int x;
-        public int y;
-
-        public Cell() {
-            this.x = 0;
-            this.y = 0;
+    // 思路:http://blog.csdn.net/doc_sgl/article/details/11832965
+    public int largestRectangleArea(int[] heights) {
+        // 84. Largest Rectangle in Histogram
+        // https://leetcode.com/problems/largest-rectangle-in-histogram/
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        int maxArea = 0;
+        int len = heights.length;
+        while (i <= len) {
+            int nextHeight = (i == len ? 0 : heights[i]);
+            if (stack.empty() || heights[stack.peek()] <= nextHeight) {
+                stack.push(i);
+                i++;
+            } else {
+                int t = stack.pop();
+                maxArea = Math.max(maxArea, heights[t] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+            }
         }
+        return maxArea;
     }
 
     public boolean isOne(char a) {
@@ -23,6 +36,39 @@ public class Solution {
 
 
     public int maximalRectangle(char[][] matrix) {
-      return 0;
+        int res = 0;
+        int m = matrix.length;
+        if(m == 0) return 0;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for(int j=0;j<n;j++){
+            if(isOne(matrix[0][j])) dp[0][j]=1;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(isOne(matrix[i][j])){
+                    dp[i][j] = dp[i-1][j]+1;
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            int tempRes = largestRectangleArea(dp[i]);
+            res = Math.max(tempRes,res);
+        }
+      return res;
+    }
+
+    @Test
+    public void test(){
+        char[][] matrix = new char[4][5];
+        matrix[0] = new char[]{'1', '0', '1', '0', '0'};
+        matrix[1] = new char[]{'1', '0', '1', '1', '1'};
+        matrix[2] = new char[]{'1', '1', '1', '1', '1'};
+        matrix[3] = new char[]{'1', '0', '0', '1', '0'};
+        System.out.println(maximalRectangle(matrix));
     }
 }
