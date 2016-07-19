bbfd118b2f9f06157c1f2c07abad59635ce72d98
lilelr <lilelr@163.com>
2016-07-19 15:47:37 +0800

    279. Perfect Squares db BFS

---
+++ src/note/dp/PerfectSquares279/Solution.java
@@ -0,0 +1,11 @@
+package note.dp.PerfectSquares279;
+
+/**
+ * Created by yuxiao on 7/19/16.
+ */
+public class Solution {
+    //https://discuss.leetcode.com/topic/24255/summary-of-4-different-solutions-bfs-dp-static-dp-and-mathematics/2
+//    public int numSquares(int n) {
+//
+//    }
+}
--- src/note/dp/Triangle120/Solution.java
+++ src/note/dp/Triangle120/Solution.java
@@ -23,6 +23,31 @@ public class Solution {
         return nums[0];
     }
 
+    // Top-Down
+    public int minimumTotalTop_Down(List<List<Integer>> triangle) {
+        if(triangle.size() == 0) return 0;
+        long[][] cache = new long[triangle.size()][triangle.size()];
+        for(int i = 0; i < cache.length; i++)
+            for(int j = 0; j < cache.length; j++)
+                cache[i][j] = Long.MIN_VALUE;
+        return dfs(triangle, 0, 0, 0, cache);
+    }
+
+    private int dfs(List<List<Integer>> triangle, int iCur, int jCur, int total, long[][] cache){
+        int min = Integer.MAX_VALUE;
+        if(cache[iCur][jCur] != Long.MIN_VALUE){
+            min = (int)cache[iCur][jCur];
+        }
+        else if(iCur == triangle.size() - 1){
+            min = triangle.get(iCur).get(jCur);
+        }else{
+            min = dfs(triangle, iCur + 1, jCur, triangle.get(iCur).get(jCur), cache);
+            min = Math.min(min, dfs(triangle, iCur + 1, jCur + 1, triangle.get(iCur).get(jCur), cache));
+        }
+        cache[iCur][jCur] = min;
+        return min + total;
+    }
+
 
 
 }
