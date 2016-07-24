905b5f92fedb3256b872e8ef62dad3c86e1e0551
lilelr <lilelr@163.com>
2016-07-24 14:11:15 +0800

    summary

--- src/note/dp/BestTimetoBuyandSellStockII122/Solution.java
+++ src/note/dp/BestTimetoBuyandSellStockII122/Solution.java
@@ -7,16 +7,16 @@ public class Solution {
     //greedy
     public int maxProfit(int[] prices) {
         int len = prices.length;
-        if(len<=1) return 0;
-        if(len ==2){
-            return prices[0]<prices[1]? (prices[1]-prices[0]):0;
+        if (len <= 1) return 0;
+        if (len == 2) {
+            return prices[0] < prices[1] ? (prices[1] - prices[0]) : 0;
         }
 
         int lastbuy = prices[0];
-        int profit=0;
-        for(int i=1;i<len;i++){
-            if(prices[i]>lastbuy){
-                profit = profit + prices[i]-lastbuy;
+        int profit = 0;
+        for (int i = 1; i < len; i++) {
+            if (prices[i] > lastbuy) {
+                profit = profit + prices[i] - lastbuy;
             }
             lastbuy = prices[i];
         }
--- src/note/dp/BestTimetoBuyandSellStockIII123/Solution.java
+++ src/note/dp/BestTimetoBuyandSellStockIII123/Solution.java
@@ -10,65 +10,72 @@ public class Solution {
 //    Idea is simple : Keep track of the minimum value till previous day and
 //    check how much maximum profit can be obtained for current day.
 
-    // for example find the max profit of [1,5,3,6,4] at most at two trancactions.
+    // for example find the max profit of [1,5,3,6,4] at most at two transactions.
     // i=1 min = Math.min(min,price[j-1])
-     // dp[1][j] = Math.max(dp[1][j-1],price[j]-min);
+    // dp[1][j] = Math.max(dp[1][j-1],price[j]-min);
     //
-    // in trans 2, dp[1][1] = 4
+    //         1,  5,   3,   6,   4
+    //index    [0] [1]  [2]
+    //
+    //
+    // in trans 2, dp[1][1] = 4,dp[1][2]=4
     // in order to add the max profit of the first trans, we let 4 reduced by 3 (3-4=-1)
     // so min = (min,-1) =-1
     // so after two trans ,the max profit is 6-(-1)=7
     // i=2 min = Math.min(min,price[j-1] - dp[1][j-1]);
     // dp [2][j] = Math.max(dp[2][j-1],prices[j]-min);
     public int maxProfit(int[] prices) {
-        if(prices.length<2) return 0;
-        int trans=2;
+        if (prices.length < 2) return 0;
+        int trans = 2;
         int len = prices.length;
-        int[][] dp = new int[trans+1][len];
-        for(int i=1;i<=trans;i++){
+        int[][] dp = new int[trans + 1][len];
+        for (int i = 1; i <= trans; i++) {
             int min = Integer.MAX_VALUE;
-            for(int j=1;j<len;j++){
-                min = Integer.min(min,prices[j-1]-dp[i-1][j-1]);
-                dp[i][j] = Integer.max(dp[i][j-1],prices[j] - min);
+            for (int j = 1; j < len; j++) {
+                min = Integer.min(min, prices[j - 1] - dp[i - 1][j - 1]);
+                dp[i][j] = Integer.max(dp[i][j - 1], prices[j] - min);
             }
         }
-        return dp[trans][len-1];
+        return dp[trans][len - 1];
     }
 
 
     // The second method. forward and backward iterative
     public int maxProfit2(int[] prices) {
         int len = prices.length;
-        if(len<2) return 0;
+        if (len < 2) return 0;
 
+        // array forward keeps the max profit of the prefix array
         int[] forward = new int[len];
-        forward[0]=0;
+        forward[0] = 0;
         int min = prices[0];
-        for(int j=1;j<len;j++){
-            min = Integer.min(min,prices[j-1]);
-            forward[j] = Integer.max(forward[j-1],prices[j]-min);
+        for (int j = 1; j < len; j++) {
+            min = Integer.min(min, prices[j - 1]);
+            forward[j] = Integer.max(forward[j - 1], prices[j] - min);
         }
 
+        // array backward keeps the max profit of the suffix array
         int[] backward = new int[len];
-        backward[len-1]=0;
-        int max=prices[len-1];
-        for(int i=len-2;i>=0;i--){
-            max = Integer.max(max,prices[i]);
-            backward[i] = Integer.max(backward[i+1],max-prices[i]);
+        backward[len - 1] = 0;
+        int max = prices[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            max = Integer.max(max, prices[i]);
+            backward[i] = Integer.max(backward[i + 1], max - prices[i]);
         }
 
         int res = 0;
-        for(int k=0;k<len;k++){
-            res  =Math.max(res,forward[k]+backward[k]);
+        // array forward represents the first transaction,
+        // while array backward represents the second transaction.
+        for (int k = 0; k < len; k++) {
+            res = Math.max(res, forward[k] + backward[k]);
         }
         return res;
     }
 
 
-
     @Test
-    public void test(){
-        int[] prices = {7,1,5,3,6,4};
+    public void test() {
+        int[] prices = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit2(prices));
     }
 
