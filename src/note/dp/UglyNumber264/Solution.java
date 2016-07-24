0c0f106c24adbef8411619326ef573d8ea02e1ed
lilelr <lilelr@163.com>
2016-07-24 21:31:40 +0800

    summary

--- src/note/dp/UglyNumber264/Solution.java
+++ src/note/dp/UglyNumber264/Solution.java
@@ -10,41 +10,41 @@ import java.util.*;
 public class Solution {
 
 
-
     /// Dynamic Programming  O(n)
     public int nthUglyNumber(int n) {
-        if(n==1) return 1;
+        if (n == 1) return 1;
         List<Integer> ans = new ArrayList<>();
         ans.add(1);
-        int i2=0,i3=0,i5=0;
-        int count=1;
-        while(count<=n){
-            boolean flag=true;
-            int nextItem = Math.min(ans.get(i2)*2,Math.min(ans.get(i3)*3,ans.get(i5)*5));
-            if(nextItem == ans.get(i2)*2){
+        int i2 = 0, i3 = 0, i5 = 0;
+        int count = 1;
+        while (count <= n) {
+            boolean flag = true;
+            int nextItem = Math.min(ans.get(i2) * 2, Math.min(ans.get(i3) * 3, ans.get(i5) * 5));
+            if (nextItem == ans.get(i2) * 2) {
+                //In the event that nextItem==ans.get(i2)*2, pointer move to next stop.
                 i2++;
             }
-            if(nextItem == ans.get(i3)*3){
+            if (nextItem == ans.get(i3) * 3) {
                 i3++;
 
             }
-            if(nextItem == ans.get(i5)*5){
+            if (nextItem == ans.get(i5) * 5) {
                 i5++;
 
             }
 
-                count++;
-                ans.add(nextItem);
+            count++;
+            ans.add(nextItem);
 
 
         }
-        return ans.get(n-1);
+        return ans.get(n - 1);
     }
 
     //313 58ms  O(kN)
     public int nthSuperUglyNumber(int n, int[] primes) {
 
-        if(n==1) return 1;
+        if (n == 1) return 1;
 //        Queue<Long> queue = new PriorityQueue<>();
 //        queue.offer(Long.valueOf(1));
 //        Long pollItem = 0L;
@@ -62,19 +62,19 @@ public class Solution {
         List<Integer> ans = new ArrayList<>();
         ans.add(1);
         int[] statusPoints = new int[primes.length];
-        int count=1;
-        while (count<=n){
+        int count = 1;
+        while (count <= n) {
             int nextItem = Integer.MAX_VALUE;
 
-            for(int i=0;i<statusPoints.length;i++){
-                int compareItem = ans.get(statusPoints[i])*primes[i];
-                if(compareItem < nextItem ){
+            for (int i = 0; i < statusPoints.length; i++) {
+                int compareItem = ans.get(statusPoints[i]) * primes[i];
+                if (compareItem < nextItem) {
                     nextItem = compareItem;
                 }
             }
 
-            for(int i=0;i<statusPoints.length;i++){
-                if( ans.get(statusPoints[i])*primes[i] == nextItem){
+            for (int i = 0; i < statusPoints.length; i++) {
+                if (ans.get(statusPoints[i]) * primes[i] == nextItem) {
                     statusPoints[i]++;
                 }
             }
@@ -82,40 +82,40 @@ public class Solution {
             ans.add(nextItem);
         }
 
-        return ans.get(n-1);
+        return ans.get(n - 1);
     }
 
     // optimize 31ms   O(kN)
     public int nthSuperUglyNumber2(int n, int[] primes) {
-        int [] res = new int[n];
+        int[] res = new int[n];
         res[0] = 1;
-        int [] cur = new int[primes.length];
+        int[] cur = new int[primes.length];
 
-        for(int i = 1; i < n; i++){
+        for (int i = 1; i < n; i++) {
             res[i] = Integer.MAX_VALUE;
-            for(int j = 0; j < primes.length; j++){
-                if (primes[j] * res[cur[j]] == res[i-1]) {
+            for (int j = 0; j < primes.length; j++) {
+                if (primes[j] * res[cur[j]] == res[i - 1]) {
                     cur[j]++;
                 }
-                res[i] = Math.min(res[i], primes[j]*res[cur[j]]);
+                res[i] = Math.min(res[i], primes[j] * res[cur[j]]);
             }
         }
-        return res[n-1];
+        return res[n - 1];
     }
 
 
     /// O(nlogk)  PriorityQueue
-    public int uglyNumberByPriorityQueue(int n){
-        if(n == 1) return 1;
-        int[] factors = {2,3,5};
+    public int uglyNumberByPriorityQueue(int n) {
+        if (n == 1) return 1;
+        int[] factors = {2, 3, 5};
         Queue<Long> queue = new PriorityQueue<>();
         queue.offer(Long.valueOf(1));
-        Long pollItem=0L;
-        for (int i=0;i<n;i++){
-             pollItem = queue.poll();
-            for(int factor:factors){
-                Long addItem = pollItem*factor;
-                if(!queue.contains(addItem)){
+        Long pollItem = 0L;
+        for (int i = 0; i < n; i++) {
+            pollItem = queue.poll();
+            for (int factor : factors) {
+                Long addItem = pollItem * factor;
+                if (!queue.contains(addItem)) {
                     queue.offer(addItem);
                 }
             }
@@ -124,9 +124,8 @@ public class Solution {
     }
 
 
-
     @Test
-    public void testnthUglyNumber(){
+    public void testnthUglyNumber() {
 
         System.out.println(uglyNumberByPriorityQueue(12));
     }
