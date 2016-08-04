2f5b13700b5c5bb6760b2dc01dcbfbd9fb84d5cd
lilelr <lilelr@163.com>
2016-08-04 17:35:31 +0800

    212.Word Search II  trie + dfs + backtracking  hard

--- src/note/tree/trie/WordSearchII212/Solution.java
+++ src/note/tree/trie/WordSearchII212/Solution.java
@@ -1,12 +1,98 @@
 package note.tree.trie.WordSearchII212;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by yuxiao on 8/4/16.
+ * https://leetcode.com/problems/word-search-ii/
+ * trie + dfs + backtracking
  */
+class TrieNode {
+    public TrieNode[] children = new TrieNode[26];
+    public String item = "";
+
+    public TrieNode() {
+
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.item = word;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) return false;
+            node = node.children[c - 'a'];
+        }
+        return node.item.equals(word);
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) return false;
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
 public class Solution {
+
+    private Set<String> res = new HashSet<>();
+
     public List<String> findWords(char[][] board, String[] words) {
-        return null;
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, visited, "", i, j, trie);
+            }
+        }
+        return new ArrayList<>(res);
+    }
+
+    public void dfs(char[][] board, boolean[][] visited, String str, int x, int y, Trie trie) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return;
+        if (visited[x][y]) return;
+
+        str += board[x][y];
+        if (!trie.startsWith(str)) return;
+
+        if (trie.search(str)) {
+            res.add(str);
+        }
+
+        visited[x][y] = true;
+        dfs(board, visited, str, x - 1, y, trie);
+        dfs(board, visited, str, x + 1, y, trie);
+        dfs(board, visited, str, x, y - 1, trie);
+        dfs(board, visited, str, x, y + 1, trie);
+        visited[x][y] = false;
     }
 }
