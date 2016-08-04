ad725df46f415c5ec2354e8374b46301db155451
lilelr <lilelr@163.com>
2016-08-04 17:13:47 +0800

    78 Word Search  backtracking

--- src/note/backtracking/WordSearch79/Solution.java
+++ src/note/backtracking/WordSearch79/Solution.java
@@ -2,103 +2,52 @@ package note.backtracking.WordSearch79;
 
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by yuxiao on 8/4/16.
+ * https://leetcode.com/problems/word-search/
  */
 public class Solution {
-    public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                Set<String> path = new HashSet<>();
-                if(board[i][j] == word.charAt(0)){
-                    path.add(i+","+j);
-                    if(dfs(m,n,board,i,j,word,0,word.length(),path)){
-                        return true;
-                    }
+    // https://discuss.leetcode.com/topic/47611/8ms-beats-96-13-java-solution-easy-to-understand
+    public boolean exist(char[][] board, String words) {
+        char[] word = words.toCharArray();
+        for(int i = 0; i<board.length;i++){
+            for(int j = 0; j<board[0].length;j++){
+                if(board[i][j] == word[0]){
+                    if(helper(board, i, j, word,0)){return true;}
                 }
-            }
-        }
-
-        return  false;
-    }
-
-    public boolean dfs(int m,int n,char[][] board,int i,int j,String word,int pos,int len,Set<String> path){
-        if(pos == len-1){
-            return true;
-        }
-        String up,down,left,right;
-        if(i>=1){
-             up = (i-1) + ","+j;
-        }else{
-            up= null;
-        }
-        if(i<=m-2){
-             down = (i+1)+","+j;
-        }else{
-            down = null;
-        }
-        if(j>=1){
-             left = i+","+ (j-1);
-        }else{
-            left = null;
-        }
-        if(j<=n-2){
-             right = i+","+(j+1);
-        }else{
-            right = null;
-        }
-
-        if(!path.contains(up) && up!=null){
-            if(board[i-1][j] == word.charAt(pos+1)){
-                path.add(up);
-                if(dfs(m,n,board,i-1,j,word,pos+1,len,path)){
-                    return true;
-                }
-            }
-        }
-
-        if(!path.contains(down) && down!=null){
-            if(board[i+1][j] == word.charAt(pos+1)){
-                path.add(down);
-                if(dfs(m,n,board,i+1,j,word,pos+1,len,path)){
-                    return true;
-                }
-            }
-        }
-
-        if(!path.contains(left) && left!=null){
-            if(board[i][j-1] == word.charAt(pos+1)){
-                path.add(left);
-                if(dfs(m,n,board,i,j-1,word,pos+1,len,path)){
-                    return true;
-                }
-            }
-        }
-
-        if(!path.contains(right) && right!=null){
-            if(board[i][j+1] == word.charAt(pos+1)){
-                path.add(right);
-                if(dfs(m,n,board,i,j+1,word,pos+1,len,path)){
-                    return true;
-                }
-            }
-        }
-
+            }//for j
+        }//for i
         return false;
+    }//exist
 
+    // check if board[i][j] is equal to word[index] first inside the function
+    // then decide the next direction to go head
+    public boolean helper(char[][] board, int i, int j, char[] word,int index){
+        if(index == word.length)return true;
+        if(i<0 || j<0 || i == board.length || j == board[i].length) return false;
+        if(board[i][j] != word[index])return false;
 
-    }
+        // in order not to go back , for example 'a'(94) ^ 256 = 353 ,next visit 353 will not
+        // equal to any letter of word
+        board[i][j] ^= 256;
+
+        // code trick using "||" ,it is absolutely cool
+        boolean exist = helper(board, i, j+1, word, index+1)
+                || helper(board, i-1, j, word, index+1)
+                || helper(board, i, j-1, word, index+1)
+                || helper(board, i+1, j, word, index+1);
+
+        board[i][j] ^= 256;
+        return exist;
+
+    }//helper
     @Test
-    public void test(){
+    public void test() {
         char[][] board = new char[1][2];
-        board[0][0]= 'a';
-        board[0][1]= 'a';
-        boolean res = exist(board,"aaa");
+        board[0][0] = 'a';
+        board[0][1] = 'a';
+        boolean res = exist(board, "aaa");
         System.out.println(res);
     }
 }
