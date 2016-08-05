f4e498894e65db767d9c01182d9ba06cf21057a1
lilelr <lilelr@163.com>
2016-08-05 11:40:56 +0800

    94. Binary Tree Inorder Traversal Morris traversal algorithm complex O(n) space complex O(l)

--- src/note/tree/BinaryTreeInorderTraversal94/Solution.java
+++ src/note/tree/BinaryTreeInorderTraversal94/Solution.java
@@ -25,12 +25,43 @@ public class Solution {
         return res;
     }
 
+    // recursive space complex O(n) because of recursive stack
     public void inOrder(TreeNode root,List<Integer> res){
         if(root!=null){
             inOrder(root.left,res);
             res.add(root.val);
             inOrder(root.right,res);
         }
+    }
+
+    //  Morris traversal algorithm complex O(n) space complex O(l)
+    public List<Integer> inorderTraversal2(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        TreeNode curr = root;
+        while (curr!=null){
+            if(curr.left == null){
+                list.add(curr.val);
+                curr = curr.right;
+            }else{
+                TreeNode predecessor = curr.left;
+                while (predecessor.right != null && predecessor.right!=curr){
+                    predecessor = predecessor.right;
+                }
+
+                if(predecessor.right == null){
+                    predecessor.right = curr;
+                    curr = curr.left;
+                }
+
+                else{
+                    predecessor.right = null;
+                    list.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return list;
     }
 
     @Test
