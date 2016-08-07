7d90cb6c42bf2c35afe796235e20e51ebe7ac03f
lilelr <lilelr@163.com>
2016-08-07 23:17:15 +0800

    307. Range Sum Query - Mutable   Segment Tree  classic

---
+++ src/note/tree/segment_tree/NumArray.java
@@ -0,0 +1,92 @@
+package note.tree.segment_tree;
+
+/**
+ * Created by yuxiao on 8/7/16.
+ * https://leetcode.com/problems/range-sum-query-mutable/
+ */
+public class NumArray {
+    //https://discuss.leetcode.com/topic/29918/17-ms-java-solution-with-segment-tree/13
+    //http://www.geeksforgeeks.org/segment-tree-set-1-sum-of-given-range/
+    class SegmentTreeNode {
+        int start, end;
+        SegmentTreeNode left, right;
+        int sum;
+
+        public SegmentTreeNode(int start, int end) {
+            this.start = start;
+            this.end = end;
+            this.left = null;
+            this.right = null;
+            this.sum = 0;
+        }
+    }
+
+    SegmentTreeNode root = null;
+
+    public NumArray(int[] nums) {
+        root = buildTree(nums, 0, nums.length - 1);
+    }
+
+    // recursive to build segment tree, it costs O(2n+1)
+    private SegmentTreeNode buildTree(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        } else {
+            SegmentTreeNode ret = new SegmentTreeNode(start, end);
+            if (start == end) {
+                ret.sum = nums[start];
+            } else {
+                int mid = start + (end - start) / 2;
+                ret.left = buildTree(nums, start, mid);
+                ret.right = buildTree(nums, mid + 1, end);
+                ret.sum = ret.left.sum + ret.right.sum;
+            }
+            return ret;
+        }
+    }
+
+    void update(int i, int val) {
+        update(root, i, val);
+    }
+
+    //  recursive to update value, too O(logn)
+    void update(SegmentTreeNode root, int pos, int val) {
+        if (root.start == root.end) {
+            root.sum = val;
+        } else {
+            int mid = root.start + (root.end - root.start) / 2;
+            if (pos <= mid) {
+                update(root.left, pos, val);
+            } else {
+                update(root.right, pos, val);
+            }
+            root.sum = root.left.sum + root.right.sum;
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sumRange(root, i, j);
+    }
+
+    // O(logn)
+    public int sumRange(SegmentTreeNode root, int start, int end) {
+        if (root.end == end && root.start == start) {
+            return root.sum;
+        } else {
+            int mid = root.start + (root.end - root.start) / 2;
+            if (end <= mid) {
+                return sumRange(root.left, start, end);
+            } else if (start >= mid + 1) {
+                return sumRange(root.right, start, end);
+            } else {
+                return sumRange(root.right, mid + 1, end) + sumRange(root.left, start, mid);
+            }
+        }
+    }
+}
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.update(1, 10);
+// numArray.sumRange(1, 2);
