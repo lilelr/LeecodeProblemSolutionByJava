50dc4ab8827015319387ef9e926e45cdc53c9a7c
lilelr <lilelr@163.com>
2016-08-09 20:47:58 +0800

    3 Longest Substring Without Repeating Characters  HashMap

--- src/note/hashmap/LongestSubstringWithoutRepeatingCaharacters3/Solution.java
+++ src/note/hashmap/LongestSubstringWithoutRepeatingCaharacters3/Solution.java
@@ -8,55 +8,13 @@ import java.util.Map;
 /**
  * Created by yuxiao on 8/9/16.
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/
- *
  */
 public class Solution {
-//    public int lengthOfLongestSubstring(String s) {
-//        if (s.length() == 0) return 0;
-//        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-//        int max = 0;
-//        for (int i = 0, j = 0; i < s.length(); ++i) {
-//            if (map.containsKey(s.charAt(i))) {
-//                j = Math.max(j, map.get(s.charAt(i)) + 1);
-//            }
-//            map.put(s.charAt(i), i);
-//            max = Math.max(max, i - j + 1);
-//        }
-//        return max;
-//    }
-
-//    public int lengthOfLongestSubstring(String s) {
-//        /*
-//        **O(n^2) worest case run time, happens when there is no repeating characters in the string
-//        **O(1) space
-//        **keep 2 pointers for the start and end of the substring
-//        */
-//        if (s == null || s.equals("")) return 0;
-//
-//        //actually do not need to keep the start and end of the substring. Can only use one int value to keep the longest length of the substring
-//        int start = 0;
-//        int end = 0;
-//        int tmpstart = 0;
-//        int tmpend = 1;
-//        for (; tmpend < s.length(); tmpend++){
-//            for (int i = tmpstart; i  < tmpend; i++) {
-//                if (s.charAt(tmpend) == s.charAt(i)) {
-//                    tmpstart = i + 1;
-//                }
-//            }
-//            if (tmpend - tmpstart + 1 > end - start + 1) {
-//                start = tmpstart;
-//                end = tmpend;
-//            }
-//        }
-//
-//        return end - start + 1;
-//    }
 
     //https://discuss.leetcode.com/topic/50224/java-easy-to-understand-dp-solution-with-detailed-explanation
+    // O(n)
     public int lengthOfLongestSubstring(String s) {
 
-        //it is DP problem with O(n)
         int l = s.length();
         int maxl = 0;
         int dupindex = -1;
