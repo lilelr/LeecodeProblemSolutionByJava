565e80f782fa3931e300f60aa202923e6aa02fc0
lilelr <lilelr@163.com>
2016-08-12 23:42:19 +0800

    138 Copy List with Random Pointer  classic ,make the minimus space complexity A solution with constant space complexity O(1) and linear time complexity O(N)

---
+++ src/note/LinkList/CopyListwithRandomPointer138/Solution.java
@@ -0,0 +1,66 @@
+package note.LinkList.CopyListwithRandomPointer138;
+
+/**
+ * Created by yuxiao on 8/12/16.
+ */
+
+// Definition for singly-linked list with a random pointer.
+//https://leetcode.com/problems/copy-list-with-random-pointer/
+class RandomListNode {
+    int label;
+    RandomListNode next, random;
+
+    RandomListNode(int x) {
+        this.label = x;
+    }
+};
+
+public class Solution {
+    //https://discuss.leetcode.com/topic/7594/a-solution-with-constant-space-complexity-o-1-and-linear-time-complexity-o-n
+    //A solution with constant space complexity O(1) and linear time complexity O(N)
+    public RandomListNode copyRandomList(RandomListNode head) {
+        RandomListNode iter = head, next;
+
+        // First round: make copy of each node,
+        // and link them together side-by-side in a single list.
+        while (iter != null) {
+            next = iter.next;
+
+            RandomListNode copy = new RandomListNode(iter.label);
+            iter.next = copy;
+            copy.next = next;
+
+            iter = next;
+        }
+
+        // Second round: assign random pointers for the copy nodes.
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+
+        // Third round: restore the original list, and extract the copy list.
+        iter = head;
+        RandomListNode pseudoHead = new RandomListNode(0);
+        RandomListNode copy, copyIter = pseudoHead;
+
+        while (iter != null) {
+            next = iter.next.next;
+
+            // extract the copy
+            copy = iter.next;
+            copyIter.next = copy;
+            copyIter = copy;
+
+            // restore the original list
+            iter.next = next;
+
+            iter = next;
+        }
+
+        return pseudoHead.next;
+    }
+}
