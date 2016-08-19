20cfde2668cb99f0069411c7abfebfd23950b613
lilelr <lilelr@163.com>
2016-08-19 08:41:42 +0800

    clone graph 133 summary

--- src/note/BFS/CloneGraph133/Solution.java
+++ src/note/BFS/CloneGraph133/Solution.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * Created by yuxiao on 6/14/16.
+ * https://leetcode.com/problems/clone-graph/
  */
 public class Solution {
     // https://leetcode.com/problems/clone-graph/
@@ -28,18 +29,21 @@ public class Solution {
 
     public UndirectedGraphNode bfs(UndirectedGraphNode node){
         if(node ==null) return null;
-
+        // queue  stores the nodes of old map
         Deque<UndirectedGraphNode> queue = new LinkedList<>();
         queue.push(node);
+        // map respresents the clone map
         Map<Integer,UndirectedGraphNode> map = new HashMap<>();
         map.put(node.label,new UndirectedGraphNode(node.label));
         while (!queue.isEmpty()){
             UndirectedGraphNode frontNode = queue.pop();
+            // consider the adjcent nodes in priority
             for(UndirectedGraphNode neighbor: frontNode.neighbors){
                 if (!map.containsKey(neighbor.label)){
                     map.put(neighbor.label,new UndirectedGraphNode(neighbor.label));
                     queue.push(neighbor);
                 }
+                // add the line
                 map.get(frontNode.label).neighbors.add(map.get(neighbor.label));
             }
         }
