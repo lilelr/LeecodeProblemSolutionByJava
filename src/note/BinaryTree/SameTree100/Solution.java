3489d40d77e9bfbb88abf9d66308238de741befe
lilelr <lilelr@163.com>
2016-08-22 21:22:56 +0800

    binary tree  summary

---
+++ src/note/BinaryTree/SameTree100/Solution.java
@@ -0,0 +1,43 @@
+package note.BinaryTree.SameTree100;
+
+/**
+ * Created by yuxiao on 16/4/13.
+ * https://leetcode.com/problems/same-tree/
+ */
+public class Solution {
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    boolean preOrder(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if ((p == null && q != null) || (q == null && p != null)) {
+            return false;
+        } else {
+            if (p.val != q.val) {
+                return false;
+            } else {
+                boolean isLeftSame = preOrder(p.left, q.left);
+                if (isLeftSame == true) {
+                    return preOrder(p.right, q.right);
+                } else {
+                    return false;
+                }
+            }
+
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return preOrder(p, q);
+    }
+}
