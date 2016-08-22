3de9fcc66151bbf2d4b7a2e9321306dd819b1ef2
lilelr <lilelr@163.com>
2016-08-22 12:11:35 +0800

    HashMap summary

---
+++ src/note/hashmap/LongestConsecutiveSequence128/Solution.java
@@ -0,0 +1,49 @@
+package note.hashmap.LongestConsecutiveSequence128;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+
+/**
+ * Created by yuxiao on 16/4/12.
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ */
+public class Solution {
+    // O(n) 19ms
+    public int longestConsecutive(int[] nums) {
+        int len = nums.length;
+        if (len == 0) return 0;
+        if (len == 1) return 1;
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int item : nums) {
+            set.add(item);
+        }
+        Integer[] array = (Integer[]) set.toArray(new Integer[0]);
+        int maxlen = 0;
+        for (Integer item : array) {
+            int count = 0;
+            if (set.contains(item + 1)) {
+                continue;
+            } else {
+                while (set.remove(item--)) {
+                    count++;
+                }
+                maxlen = Math.max(maxlen, count);
+            }
+        }
+
+        return maxlen;
+
+    }
+
+
+    @Test
+    public void testlongestConsecutivei() {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int ans = longestConsecutive(nums);
+        System.out.println(ans);
+        System.out.println(new Integer[0]);
+    }
+
+}
